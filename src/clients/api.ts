be7874ddf6c323a3af0ceb422e54668d696dfb89
Frazier <mrfrazier628@hotmail.com>
2025-12-10 13:46:59 -0500

    updated projects page

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export const apiClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     headers: {
-        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFjYTRiNGEzMDkwNWRjZjhjMTZjMiIsInVzZXJuYW1lIjoibGVta2JtbGEiLCJlbWFpbCI6InRpbUBnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjUzOTEwNDAsImV4cCI6MTc2NTQ3NzQ0MH0.VSS2guPz0RIMenZo9BGCzOtOBBmC6V38AznClCrzUxg"
+        Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzFjYTRiNGEzMDkwNWRjZjhjMTZjMiIsInVzZXJuYW1lIjoibGVta2JtbGEiLCJlbWFpbCI6InRpbUBnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjUzOTIzNzgsImV4cCI6MTc2NTQ3ODc3OH0.2GwSSnGl5PxOqLyVXNpSk5HV78O0MmP-h1pqsaG7htM"
     }
 });
